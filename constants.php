5868e1c54f246b39160d3067141e6ddc2701df09
Matt Connolly <mjc12@cornell.edu>
2018-02-26 18:01:03 -0500

    Add constants file with links to Mann services

---
+++ constants.php
@@ -0,0 +1,10 @@
+<?php
+
+global $libraries_url;
+$libraries_url = 'http://mannservices.mannlib.cornell.edu/LibServices/showLocationInfo.do?output=json';
+
+global $courses_url;
+$courses_url = 'http://mannservices.mannlib.cornell.edu/LibServices/showCourseReserveList.do?output=json&library=';
+
+global $reserves_url;
+$reserves_url = 'http://mannservices.mannlib.cornell.edu/LibServices/showCourseReserveItemInfo.do?output=json&courseid=';
