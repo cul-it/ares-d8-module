0e617aa4e6ad43e44ccbb2a095ccf2923ed12f0b
Matt Connolly <mjc12@cornell.edu>
2019-01-23 14:25:32 -0500

    Bump to v1.0 for release; update Ares web service URLs

--- modules/ares/constants.php
+++ modules/ares/constants.php
@@ -1,10 +1,10 @@
 <?php
 
 global $libraries_url;
-$libraries_url = 'http://mannservices.mannlib.cornell.edu/LibServices/showLocationInfo.do?output=json';
+$libraries_url = 'http://api.library.cornell.edu/LibServices/showLocationInfo.do?output=json';
 
 global $courses_url;
-$courses_url = 'http://mannservices.mannlib.cornell.edu/LibServices/showCourseReserveList.do?output=json&library=';
+$courses_url = 'http://api.library.cornell.edu/LibServices/showCourseReserveList.do?output=json&library=';
 
 global $reserves_url;
-$reserves_url = 'http://mannservices.mannlib.cornell.edu/LibServices/showCourseReserveItemInfo.do?output=json&courseid=';
+$reserves_url = 'http://api.library.cornell.edu/LibServices/showCourseReserveItemInfo.do?output=json&courseid=';
